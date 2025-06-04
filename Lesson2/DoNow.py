569c189250a89efc7aff545c46bb72948cde691d
Mayen819 <maenjok@gmail.com>
2025-06-04 17:29:44 -0500

    Update DoNow.py

---
+++ Lesson2/DoNow.py
@@ -0,0 +1,19 @@
+#Ex 1
+
+sandals_count = 3
+sneakers_count = 1
+boots_count = 2
+
+
+
+total_shoes = sandals_count + sneakers_count + boots_count
+
+
+
+
+#Ex 2
+
+quarters_per_dollar = 4 
+number_of_dollars =
+
+number_of_quarters = # Write code here
